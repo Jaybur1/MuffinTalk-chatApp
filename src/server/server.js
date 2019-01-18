79c51b26198145c40c6ef4ad5aa753b0e16516eb
Jay Burbyga <jayburbyga@gmail.com>
2019-01-18 17:28:22 -0500

    Fire Up

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,12 @@ const io = module.exports.io = require('socket.io')(server)
 const PORT = process.env.PORT || 3231
 
 const SocketManager = require('./SocketManager')
-app.use(express.static(__dirname + '../../build'))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '../../build'))
+}
+app.get('*', (req, res) => {
+    res.sendFile(req.join(__dirname, 'build', 'index.html'))
+})
 io.on('connection', SocketManager)
 
 server.listen(PORT, () => {
