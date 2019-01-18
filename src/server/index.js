c7ea49a269fd189f88992759c1e172ae829028db
Jay Burbyga <jayburbyga@gmail.com>
2019-01-18 18:09:38 -0500

    Fire Up

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,14 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-
 const io = module.exports.io = require('socket.io')(server)
 
 const PORT = process.env.PORT || 3231
 
 const SocketManager = require('./SocketManager')
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname + '../../build'))
-}
-app.get('*', (req, res) => {
-    res.sendFile(req.join(__dirname, 'build', 'index.html'))
-})
+app.use(express.static(__dirname + '/../../build'))
 io.on('connection', SocketManager)
 
 server.listen(PORT, () => {
-    console.log(`connected to port : ${PORT}`);
+    console.log("Connected to port:" + PORT);
 })
